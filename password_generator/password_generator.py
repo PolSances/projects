a10bad603d6157d8c6ff10d2ac92bcb9ae1af356
PolSances <polsances@gmail.com>
2024-09-02 16:26:53 +0200

    The program worls but when the random elects an undercase it prints it with ][

--- password_generator/password_generator.py
+++ password_generator/password_generator.py
@@ -5,106 +5,47 @@ choices3 = ["more", "normal", "less"]
 choices4 = ["super", "more", "normal", "less"]
 choices5 = ["mega", "super", "more", "normal", "less"]
 
-normal = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-]
+normal = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 capital = [chr(i) for i in range(65, 91)]
 numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-special_c = [
-    "!",
-    "@",
-    "#",
-    "$",
-    "%",
-    "^",
-    "&",
-    "*",
-    "(",
-    ")",
-    "-",
-    "_",
-    "=",
-    "+",
-    "[",
-    "]",
-    "{",
-    "}",
-    "|",
-    "\\",
-    ":",
-    ";",
-    '"',
-    "'",
-    "<",
-    ">",
-    ",",
-    ".",
-    "?",
-    "/",
-]
+special_c = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "|", "\\", ":", ";", "\"", "\"", "<", ">", ",", ".", "?", "/"]
 mastersec = ["ç", "ñ"]
 password = []
 
 
 def main():
-    generate_password(get_level(), get_length())
-    print(password)
+    generate_password(get_level(),get_length())
+    print_password()
 
-
-def generate_password(lev, lth):
+def generate_password(lev,lth):
     if lev == 1:
         for i in range(lth):
             password.append(normal[random.randint(0, len(normal) - 1)])
-
+    
     elif lev == 2:
         for i in range(lth):
             if random.choice["more", "less"] == "more":
                 password.append(capital[random.randint(0, len(capital) - 1)])
-            else:
+            else: 
                 password.append(normal[random.randint(0, len(normal - 1))])
 
     elif lev == 3:
         for i in range(lth):
             x = random.choice(choices3)
             if x == "more":
-                password.append(numbers[random.randint(0, len(numbers) - 1)])
+                password.append(numbers[random.randint(0, len(numbers)- 1)])
             elif x == "normal":
                 password.append(capital[random.randint(0, len(capital) - 1)])
             elif x == "less":
-                password.append(normal[random.randint(0, len(normal) - 1)])
-
+                password.append(normal[random.randint(0, len(normal) - 1 )])
+    
     elif lev == 4:
         for i in range(lth):
             x = random.choice(choices4)
             if x == "super":
-                password.append(special_c[random.randint(0, len(special_c) - 1)])
+                password.append(special_c[random.randint(0, len(special_c)- 1)])
             elif x == "more":
-                password.append(numbers[random.randint(0, len(numbers) - 1)])
+                password.append(numbers[random.randint(0, len(numbers)- 1)])
             elif x == "normal":
                 password.append(capital[random.randint(0, len(capital) - 1)])
             elif x == "less":
@@ -113,9 +54,9 @@ def generate_password(lev, lth):
         for i in range(lth):
             x = random.choice(choices5)
             if x == "mega":
-                password.append(mastersec[random.randint(0, len(mastersec) - 1)])
+                password.append(mastersec[random.randint(0,len(mastersec) - 1)])
             elif x == "super":
-                password.append(special_c[random.randint(0, len(special_c) - 1)])
+                password.append(special_c[random.randint(0, len(special_c)- 1)])
             elif x == "more":
                 password.append(numbers[random.randint(0, len(numbers) - 1)])
             elif x == "normal":
@@ -123,7 +64,7 @@ def generate_password(lev, lth):
             elif x == "less":
                 password.append([normal[random.randint(0, len(normal) - 1)]])
 
-
+        
 def get_level():
     while True:
         level = input("What level of security are you looking for? (1-5) : ")
@@ -137,7 +78,7 @@ def get_level():
             print("Enter a Number please!")
             continue
     return int(level)
-
+    
 
 def get_length():
     while True:
@@ -153,5 +94,10 @@ def get_length():
             continue
     return int(length)
 
+def print_password(): 
+    password_formatted = ""
+    for i in range(len(password)):
+        password_formatted = password_formatted + str(password[i])
 
+    print(password_formatted)
 main()
