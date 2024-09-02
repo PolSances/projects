a2764f45d5359ea6e7f8bf5561d2cc9ccdfaeb8d
PolSances <polsances@gmail.com>
2024-09-02 16:12:13 +0200

    Errors with module random fixed

--- password_generator/password_generator.py
+++ password_generator/password_generator.py
@@ -1,6 +1,11 @@
 import random
 
-normal = [chr(i) for i in range(97, 123)] 
+
+choices3 = ["more", "normal", "less"]
+choices4 = ["super", "more", "normal", "less"]
+choices5 = ["mega", "super", "more", "normal", "less"]
+
+normal = [['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']] 
 capital = [chr(i) for i in range(65, 91)]  
 numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 special_c = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', '|', '\\', ':', ';', '"', "'", '<', '>', ',', '.', '?', '/']
@@ -27,17 +32,17 @@ def generate_password(lev,lth):
 
     elif lev == 3:
         for i in range(lth - 1):
-            x = random.choice("more", "normal", "less")
+            x = random.choice(choices3)
             if x == "more":
                 password.append(numbers[random.randint(0, len(numbers)- 1)])
             elif x == "normal":
                 password.append(capital[random.randint(0, len(capital) - 1)])
             elif x == "less":
-                password.append(normal[random.randint(0, len(normal - 1))])
+                password.append(normal[random.randint(0, len(normal) - 1 )])
     
     elif lev == 4:
         for i in range(lth - 1):
-            x = random.choice("super", "more", "normal", "less")
+            x = random.choice(choices4)
             if x == "super":
                 password.append(special_c[random.randint(0, len(special_c)- 1)])
             elif x == "more":
@@ -45,20 +50,20 @@ def generate_password(lev,lth):
             elif x == "normal":
                 password.append(capital[random.randint(0, len(capital) - 1)])
             elif x == "less":
-                password.append(normal[random.randint(0, len(normal - 1))])
+                password.append(normal[random.randint(0, len(normal) - 1)])
     else:
         for i in range(lth - 1):
-            x = random.choice("mega", "super", "more", "normal", "less")
+            x = random.choice(choices5)
             if x == "mega":
-                password.append(mastersec[random.randint(0,len(mastersec - 1))])
+                password.append(mastersec[random.randint(0,len(mastersec) - 1)])
             elif x == "super":
                 password.append(special_c[random.randint(0, len(special_c)- 1)])
             elif x == "more":
                 password.append(numbers[random.randint(0, len(numbers) - 1)])
             elif x == "normal":
-                password.append(capital[random.randint(0, len(capital - 1))])
+                password.append(capital[random.randint(0, len(capital) - 1)])
             elif x == "less":
-                password.append([normal[random.randint(0, len(normal - 1))]])
+                password.append([normal[random.randint(0, len(normal) - 1)]])
 
         
 
@@ -70,18 +75,6 @@ def get_level():
                 break
             else:
                 print("Enter a Number between 1 and 5 please!")
-                continue
-        except ValueError:
-            print("Enter a Number please!")
-            continue
-    
-    while True:
-        length = input("How many characters?")
-        try:
-            if int(length) > 0:
-                break
-            else:
-                print("Enter a Number greater than 0!")
                 continue
         except ValueError:
             print("Enter a Number please!")
